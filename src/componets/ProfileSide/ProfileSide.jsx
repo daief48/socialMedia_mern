b10540fb32edbd5c7b5c82307e78c99df37d64ed
daief48 <daief20103048@gmail.com>
2023-07-27 01:21:25 +0600

    add followerCard

--- src/componets/ProfileSide/ProfileSide.jsx
+++ src/componets/ProfileSide/ProfileSide.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./ProfileSide.css";
 import LogoSearch from '../LogoSearch/LogoSearch';
 import ProfileCard from '../ProfileCard/ProfileCard';
+import FollowerCard from '../FollowerCard/FollowerCard';
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
       <LogoSearch/>
       <ProfileCard/>
+      <FollowerCard/>
     </div>
   )
 }
