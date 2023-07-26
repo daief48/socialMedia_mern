3e63ea0c510973b9e143797c3a1be70a6836eca4
daief48 <daief20103048@gmail.com>
2023-07-27 02:01:41 +0600

    PostShare added

--- src/componets/PostShare/PostShare.jsx
+++ src/componets/PostShare/PostShare.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 import ProfileImage from "../../img/profileImg.jpg";
+import {UilScenery} from "@iconscout/react-unicons";
+import {UilPlayCircle} from "@iconscout/react-unicons";
+import {UilLocationPoint} from "@iconscout/react-unicons";
+import {UilSchedule} from "@iconscout/react-unicons";
+import {UilTimes} from "@iconscout/react-unicons";
+import "./PostShare.css";
+import { useState } from 'react';
+import { useRef } from 'react';
 const PostShare = () => {
+    const [image,setImage] = useState(null);
+    const imageRef = useRef();
+    
+    const onImageChange = (event) =>{
+        if(event.target.files && event.target.files[0]){
+            let img = event.target.files[0];
+            setImage({
+                image: URL.createObjectURL(img)
+            })
+        }
+    }
   return (
     <div className='PostShare'>
       <img src={ProfileImage} alt="" srcset="" />
+      <div>
+        <input type="text" placeholder="What's happening"/>
+        <div className="postOptions">
+        <div className="option" style={{color: "var(--photo)"}} onClick={() => imageRef.current.click()}>
+            <UilScenery/>
+            Photo
+        </div>
+        <div className="option" style={{color: "var(--video)"}}>
+            <UilPlayCircle/>
+            Video
+        </div>
+        <div className="option" style={{color: "var(--location)"}}>
+            <UilLocationPoint/>
+            Location
+        </div>
+        <div className="option" style={{color: "var(--shedule)"}}>
+            <UilSchedule/>
+            Shedule
+        </div>
+        <button className='button ps-button'>
+            Share
+        </button>
+        <div style={{display:"none"}}>
+        <input type="file" name='myImage' ref={imageRef} onChange={onImageChange}/>
+        </div>
+      </div>
+
+      {image && (
+        <div className="previewImage">
+            <UilTimes onClick={() => setImage(null)}/>
+            <img src={image.image} alt="" />
+        </div>
+      )}
+      </div>
+      
     </div>
   )
 }
