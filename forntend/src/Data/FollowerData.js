966c9d7e8f1b6654426605aab5127062829817e6
daief48 <daief20103048@gmail.com>
2023-07-28 18:10:27 +0600

    add forntend seperate and backend start

---
+++ forntend/src/Data/FollowerData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+
+export const Followers = [
+    {name: "Daief Sikder",username:"daief007",img:img1},
+    {name: "Aklil Thashin",username:"afrinakil005",img:img2},
+    {name: "Thalin Faruk Akash",username:"afrin💞akash",img:img3},
+    {name: "Akik",username:"asma+akik",img:img4}
+]
---
+++ forntend/src/componets/FollowerCard/FollowerCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./FollowerCard.css";
+import { Followers } from '../../Data/FollowerData';
+const FollowerCard = () => {
+    return (
+        <div className='FollowerCard'>
+            <h3>Who is follower you</h3>
+            {Followers.map((follower, id) => {
+                return (
+                    <div className="follower">
+                    <div>
+                        <img src={follower.img} alt="" className='followerImage' />
+                        <div className="name">
+                            <span>{follower.name}</span>
+                            <span>@{follower.username}</span>
+                        </div>
+                    </div>
+                    <button className='button fc-button'>
+                        Follow
+                    </button>
+                </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default FollowerCard
