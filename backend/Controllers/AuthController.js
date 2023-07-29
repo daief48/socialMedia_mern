78ee18e7c358eb41b4396a1a44ebcc43b6ba3725
daief48 <daief20103048@gmail.com>
2023-07-30 00:58:51 +0600

    add server register and login bug

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,18 +1,26 @@
 import UserModel from "../Models/UserSchema.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Registering a new User
 export const registerUser = async (req, res) =>{
-    const {username, password, firstname,lastname} = req.body;
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPass = await bcrypt.hash(password,salt);
-
-    const newUser = new UserModel({username,password: hashedPass,firstname,lastname});
-
+    const hashedPass = await bcrypt.hash(req.body.password,salt);
+    req.body.password = hashedPass
+    const newUser = new UserModel(req.body);
+    const {username} = req.body;
     try {
-        await newUser.save();
-        res.status(200).json(newUser);
+        const oldUser = await UserModel.findOne({username});
+        if(oldUser){
+            return res.status(400).json({message:"Username is already registered!"})
+        }
+        const user = await newUser.save();
+
+        const token = jwt.sign({
+            username: user.username, id: user._id
+        }, process.env.JWT_KEY, {expiresIn:"1h"})
+        res.status(200).json({newUser,token});
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -29,7 +37,15 @@ export const loginUser = async (req, res) =>{
         if(user){
             const validity = await bcrypt.compare(password,user.password);
 
-            validity ? res.status(200).json(user): res.status(400).json("Wrong Password")
+
+            if(!validity){
+                res.status(400).json("Wrong password");
+            }else{
+                const token = jwt.sign({
+                    username: user.username, id: user._id
+                }, process.env.JWT_KEY, {expiresIn:"1h"});
+                res.status(200).json({user,token});
+            }
         }else{
             res.status(404).json("User does not exists")
         }
