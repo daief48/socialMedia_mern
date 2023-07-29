03579607d945751c94f2f756bc30d25a15ca8714
daief48 <daief20103048@gmail.com>
2023-07-29 23:33:23 +0600

    add forntend auth login and singup

--- forntend/src/pages/Auth/Auth.jsx
+++ forntend/src/pages/Auth/Auth.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
 
 const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+
+  const [data, setData] = useState({firstname:"",lastname:"",password:"",confirmpass:"",username:""});
+
+  const [confirmPass, setConfirmPass] = useState(true);
+
+  const handleChange = (e) =>{
+    setData({...data, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    if(isSignUp){
+      if(data.password !== data.confirmpass){
+        setConfirmPass(false);
+      }
+    }
+  }
+
+  const resetForm = () =>{
+    setConfirmPass(true);
+    setData({firstname:"",lastname:"",password:"",confirmpass:"",username:""})
+
+  }
   return (
     <div className="Auth">
     {/* Left Side  */}
@@ -15,23 +40,28 @@ const Auth = () => {
       </div>
 {/* Right Side  */}
       <div className="a-right">
-      <form className="infoForm authForm">
-        <h3>Sign up</h3>
+      <form className="infoForm authForm" onSubmit={handleSubmit}>
+        <h3>{isSignUp ? "Sign up":"Log In"}</h3>
 
-        <div>
+        {isSignUp && 
+          <div>
           <input
             type="text"
             placeholder="First Name"
             className="infoInput"
             name="firstname"
+            onChange={handleChange}
+            value={data.firstname}
           />
           <input
             type="text"
             placeholder="Last Name"
             className="infoInput"
             name="lastname"
+            onChange={handleChange}
+            value={data.lastname}
           />
-        </div>
+        </div>}
 
         <div>
           <input
@@ -39,28 +69,36 @@ const Auth = () => {
             className="infoInput"
             name="username"
             placeholder="Usernames"
+            onChange={handleChange}
+            value={data.username}
           />
         </div>
 
         <div>
           <input
-            type="text"
+            type="password"
             className="infoInput"
             name="password"
             placeholder="Password"
+            onChange={handleChange}
+            value={data.password}
           />
-          <input
-            type="text"
+          {isSignUp && <input
+            type="password"
             className="infoInput"
             name="confirmpass"
             placeholder="Confirm Password"
-          />
+            onChange={handleChange}
+            value={data.confirmpass}
+          />}
         </div>
-
+          <span style={{display:confirmPass?"none":"block",color:"red",fontSize:"12px",alignSelf:"flex-end",marginRight:"5px"}}>
+            * Confirm Password is not same
+          </span>
         <div>
-            <span style={{fontSize: '12px'}}>Already have an account. Login!</span>
+            <span style={{fontSize: '12px', cursor:"pointer"}} onClick={() => {setIsSignUp((pre) => !pre);resetForm()}}>{isSignUp ? "Already have an account. Login!":"Don't have an account? Sign Up"}</span>
         </div>
-        <button className="button infoButton" type="submit">Signup</button>
+        <button className="button infoButton" type="submit">{isSignUp ? "SignUp" :"Log In"}</button>
       </form>
     </div>
     </div>
